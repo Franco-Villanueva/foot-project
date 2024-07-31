267d9236a59b2adb66e3335bac6d65c89564f175
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-07-30 21:58:43 -0300

    stack

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,49 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+
+
 import Setting from '../pages/Setting';
-import Main from "./Main";
 import Home from "../pages/Home";
 
+import { FontAwesome } from 'react-native-vector-icons';
+
 const Tab = createBottomTabNavigator()
+const HomeStackNavigator = createStackNavigator()
 
 function MyTabs() {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Fav" />
-            <Tab.Screen name="Settings" component={Setting} />
+        <Tab.Navigator 
+        initialRouteName="Home"
+        screenOptions= {{
+            headerShown: false,
+            tabBarActiveTintColor:'#365E32',
+        }}
+        >
+            <Tab.Screen 
+            name="Home" 
+            component={Home}
+            options={{
+                tabBarLabel:'Home',
+                tabBarIcon: ({color,size})=>
+                    (<FontAwesome name="home" color={color} size={size} />)
+                
+            }
+            }
+            />
+
+            <Tab.Screen 
+            name="Settings" 
+            component={Setting}
+            options={{
+                tabBarLabel:'Setting',
+                tabBarIcon: ({color,size})=>
+                    (<FontAwesome name="gear" color={color} size={size} />)
+                
+            }
+            }
+            />
         </Tab.Navigator>
     )
 }
